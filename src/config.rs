7aca12afa893ac6259efb829661a2bb2ebdcfc04
Tomas Szejnfeld Sirkis <tszejnfeld@fi.uba.ar>
2025-05-21 14:32:46 -0300

    Add test testing fund_address funcitonality

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,10 @@
-use bitcoin::{Network, Txid};
 use bitvmx_bitcoin_rpc::rpc_config::RpcConfig;
 use config as settings;
-use key_manager::config::{KeyManagerConfig};
+use key_manager::config::KeyManagerConfig;
 use protocol_builder::config::ProtocolBuilderConfig;
 use serde::{self, Deserialize};
 use std::env;
+use storage_backend::storage_config::StorageConfig;
 use tracing::warn;
 
 use crate::errors::ConfigError;
@@ -13,20 +13,13 @@ static DEFAULT_ENV: &str = "development";
 static CONFIG_PATH: &str = "config";
 
 #[derive(Deserialize, Debug)]
-pub struct WalletConfig {
-    pub network: Network,
-    pub dotenv_path: String,
-    pub txid: Txid,
-    pub vouts: u64, //ToDo: Not read from config
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(deny_unknown_fields)]
+#[serde(deny_unknown_fields)] // enforce strict field compliance
 pub struct Config {
-    pub wallet: WalletConfig,
-    pub rpc: RpcConfig,
+    pub bitcoin: RpcConfig,
     pub builder: ProtocolBuilderConfig,
     pub key_manager: KeyManagerConfig,
+    pub key_storage: StorageConfig,
+    pub storage: StorageConfig,
 }
 
 impl Config {
@@ -47,7 +40,7 @@ impl Config {
     }
 
     fn parse_config(env: String) -> Result<Config, ConfigError> {
-        let config_path = format!("{}/{}.json", CONFIG_PATH, env);
+        let config_path = format!("{}/{}.yaml", CONFIG_PATH, env);
 
         let settings = settings::Config::builder()
             .add_source(config::File::with_name(&config_path))
--- src/wallet.rs
+++ src/wallet.rs
@@ -1,18 +1,19 @@
 use std::{env, rc::Rc, str::FromStr};
 
-use bitcoin::{secp256k1::SecretKey, Address, CompressedPublicKey, Network, PublicKey, Transaction, Txid};
+use bitcoin::{
+    secp256k1::SecretKey, Address, CompressedPublicKey, Network, PrivateKey, PublicKey, Txid,
+};
 use bitvmx_bitcoin_rpc::{
     bitcoin_client::{BitcoinClient, BitcoinClientApi},
     rpc_config::RpcConfig,
 };
 use dotenvy::from_path;
-use key_manager::{
-    key_manager::KeyManager,    
-};
+use key_manager::{config::KeyManagerConfig, key_manager::KeyManager, key_store::KeyStore};
 use protocol_builder::{
     builder::Protocol,
     types::{input::SighashType, InputArgs, OutputType},
 };
+use storage_backend::{storage::Storage, storage_config::StorageConfig};
 
 use crate::errors::WalletError;
 
@@ -20,15 +21,17 @@ pub struct Wallet {
     network: Network,
     client: BitcoinClient,
     key_manager: Rc<KeyManager>,
+    pk: PublicKey,
 }
 
 impl Wallet {
     pub fn new(
-        network: Network,
         bitcoin_client_config: RpcConfig,
-        key_manager: KeyManager
+        key_manager_config: KeyManagerConfig,
+        key_store_config: StorageConfig,
+        storage_config: StorageConfig,
     ) -> Wallet {
-
+        let network = bitcoin_client_config.network;
         from_path("config/.env").expect("Failed to load .env from config/");
         let client = BitcoinClient::new(
             &bitcoin_client_config.url,
@@ -36,19 +39,39 @@ impl Wallet {
             &bitcoin_client_config.password,
         )
         .unwrap();
-        let key_manager = Rc::new(key_manager);
 
-        Wallet { network, client, key_manager }
+        let keystore = KeyStore::new(Rc::new(Storage::new(&key_store_config).unwrap()));
+        let store = Rc::new(Storage::new(&storage_config).unwrap());
+        let key_manager = Rc::new(key_manager::create_key_manager_from_config(
+            &key_manager_config,
+            keystore,
+            store,
+        ).unwrap());
+
+        let secret_key = env::var("USER_SECRET_KEY").expect("USER_SECRET_KEY must be set");
+        let secret_key = SecretKey::from_str(&secret_key).unwrap();
+        let private_key = PrivateKey::new(secret_key, network);
+
+        let pk = key_manager
+            .import_private_key(&private_key.to_string())
+            .unwrap();
+
+        Wallet {
+            network,
+            client,
+            key_manager,
+            pk,
+        }
     }
 
-    pub fn fund_tx(
+    pub fn fund_address(
         &self,
         funding_txid: Txid,
         vout: u32,
         to_pubkey: PublicKey,
         amount: u64,
         fee: u64,
-    ) -> Result<Transaction, WalletError> {
+    ) -> Result<Txid, WalletError> {
         let output = self.client.get_tx_out(&funding_txid, vout)?;
         let total_amount = output.value.to_sat();
         let (change, value) = if total_amount > amount + fee {
@@ -58,12 +81,8 @@ impl Wallet {
         } else {
             panic!("Not enough funds in the funding transaction");
         };
-        
-        let user_secret_key = env::var("USER_SECRET_KEY").expect("USER_SECRET_KEY must be set");
-        let user_pubkey = self.key_manager.import_secret_key(&user_secret_key, self.network)?;
 
-
-        let external_output = OutputType::segwit_key(total_amount, &user_pubkey)?;
+        let external_output = OutputType::segwit_key(total_amount, &self.pk)?;
         let transfer_output = OutputType::segwit_key(value, &to_pubkey)?;
 
         let mut protocol = Protocol::new("transfer_tx");
@@ -79,7 +98,7 @@ impl Wallet {
             .add_transaction_output("transfer", &transfer_output)?;
 
         if change > 0 {
-            let change_output = OutputType::segwit_key(change, &user_pubkey)?;
+            let change_output = OutputType::segwit_key(change, &self.pk)?;
             protocol.add_transaction_output("transfer", &change_output)?;
         }
 
@@ -90,16 +109,14 @@ impl Wallet {
         let mut spending_args = InputArgs::new_segwit_args();
         spending_args.push_ecdsa_signature(signature)?;
 
-        let result = protocol.transaction_to_send("transfer", &[spending_args])?;
+        let tx = protocol.transaction_to_send("transfer", &[spending_args])?;
 
+        let result = self.client.send_transaction(&tx)?;
         Ok(result)
     }
 
-    pub fn get_pubkey(&self) -> Result<PublicKey, WalletError> {
-        let user_secret_key = env::var("USER_SECRET_KEY").expect("USER_SECRET_KEY must be set");
-        let user_pubkey = self.key_manager.import_secret_key(&user_secret_key, self.network)?;
-        Ok(user_pubkey)
-
+    pub fn get_pubkey(&self) -> PublicKey {
+        self.pk
     }
 
     pub fn get_secret_key(&self) -> Result<SecretKey, WalletError> {
@@ -111,12 +128,41 @@ impl Wallet {
 
     pub fn get_address(&self) -> Result<Address, WalletError> {
         let user_secret_key = env::var("USER_SECRET_KEY").expect("USER_SECRET_KEY must be set");
-        let user_pubkey = self.key_manager.import_secret_key(&user_secret_key, self.network)?;
+        let user_pubkey = self
+            .key_manager
+            .import_secret_key(&user_secret_key, self.network)?;
         let compressed_pubkey = CompressedPublicKey::from_slice(&user_pubkey.to_bytes()).unwrap();
 
         let address = Address::p2wpkh(&compressed_pubkey, self.network);
 
         Ok(address)
     }
+}
 
+#[cfg(test)]
+mod tests {
+    use crate::config::Config;
+    use super::*;
+
+    #[test]
+    fn test_fund_address() {
+        let config = Config::new().unwrap();
+
+        let wallet = Wallet::new(
+            config.bitcoin,
+            config.key_manager,
+            config.key_storage,
+            config.storage,
+        );
+
+        let funding_txid = Txid::from_str("9946702831bccfaa40c8a9018a35b8633031201ccb85ca2e9648ad5ec8892d26").unwrap(); // Replace with the correct txid
+        let vout = 10; // Replace with the correct vout
+        let to_pubkey = wallet.get_pubkey();
+        let amount = wallet.client.get_tx_out(&funding_txid, vout).unwrap().value.to_sat()/10;
+        let fee = 150;
+
+        let result = wallet.fund_address(funding_txid, vout, to_pubkey, amount, fee);
+        assert!(result.is_ok(), "Failed to fund address: {:?}", result.err());
+        println!("Transaction ID: {}", result.unwrap());
+    }
 }

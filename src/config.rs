25d92677c116f515ae924457906a05951e51e271
Martin Jonas <martinjonas@gmail.com>
2025-05-23 17:30:07 -0300

    update config

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,11 @@
 use bitvmx_bitcoin_rpc::rpc_config::RpcConfig;
 use key_manager::config::KeyManagerConfig;
-use protocol_builder::config::ProtocolBuilderConfig;
 use serde::{self, Deserialize};
 use storage_backend::storage_config::StorageConfig;
 
 #[derive(Deserialize, Debug)]
-#[serde(deny_unknown_fields)] // enforce strict field compliance
 pub struct Config {
     pub bitcoin: RpcConfig,
-    pub builder: ProtocolBuilderConfig,
     pub key_manager: KeyManagerConfig,
     pub key_storage: StorageConfig,
     pub storage: StorageConfig,

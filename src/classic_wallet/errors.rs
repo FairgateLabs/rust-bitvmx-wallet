716d3ba52dc99069ccefbd4f76307d4e1d9a05e3
H. Andres Tournour <andres.tournour@fairgate.io>
2025-09-22 12:18:32 -0300

    Merge classic wallet

---
+++ src/classic_wallet/errors.rs
@@ -0,0 +1,44 @@
+use bitvmx_bitcoin_rpc::errors::BitcoinClientError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ClassicWalletError {
+    #[error("Error while trying to build configuration")]
+    ConfigError(#[from] bitvmx_settings::errors::ConfigError),
+
+    #[error("Error with the Bitcoin client: {0}")]
+    BitcoinClientError(#[from] BitcoinClientError),
+
+    #[error("Error while trying to build protocol")]
+    ProtocolBuilderError(#[from] protocol_builder::errors::ProtocolBuilderError),
+
+    #[error("Error with the key manager")]
+    KeyManagerError(#[from] key_manager::errors::KeyManagerError),
+
+    #[error("Error with the storage backend")]
+    StoreError(#[from] storage_backend::error::StorageError),
+
+    #[error("Funding not found {0} {1}")]
+    FundingNotFound(String, String),
+
+    #[error("Funding id not allowed. {0}")]
+    FundingIdError(String),
+
+    #[error("Key identifier already exists {0}")]
+    KeyAlreadyExists(String),
+
+    #[error("Transfer in progress {0}. Confirm or Revert before using this identifier")]
+    TransferInProgress(String),
+
+    #[error("Key not found {0}")]
+    KeyNotFound(String),
+
+    #[error("Insufficient funds for: {0}")]
+    InsufficientFunds(String),
+
+    #[error("Invalid spending scripts")]
+    InvalidSpendingScripts,
+
+    #[error("Invalid partial private keys")]
+    InvalidPartialPrivateKeys,
+}
---
+++ src/classic_wallet/lib.rs
@@ -0,0 +1,3 @@
+pub mod classic_wallet;
+pub mod config;
+pub mod errors;
